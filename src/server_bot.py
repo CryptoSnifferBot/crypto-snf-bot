415020b219236bdbdfc952b48c6b96766f605574
allanjuliato <allan.juliato@gmail.com>
2021-03-12 16:22:10 -0300

    Adicionando Funçoes e configuraçoes

--- src/server_bot.py
+++ src/server_bot.py
@@ -1,24 +1,21 @@
+import config.configs as config
+
 from src.commands_bot import Commands
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 class Server():
-    def __init__(self, token):
-        self.token = token
-
-        self.main()
-
     def main(self):
         """Start the bot."""
-        dispatcher = self.token.dispatcher
+        dispatcher = config.token.dispatcher
 
         dispatcher.add_handler(CommandHandler("start", Commands.start_command))
         dispatcher.add_handler(CommandHandler("coin", Commands.coin_command))
         dispatcher.add_handler(CommandHandler("help", Commands.help_command))
 
         # Start the Bot
-        self.token.start_polling()
+        config.token.start_polling()
 
         # Run the bot until you press Ctrl-C or the process receives SIGINT,
         # SIGTERM or SIGABRT. This should be used most of the time, since
         # start_polling() is non-blocking and will stop the bot gracefully.
-        self.token.idle()
+        config.token.idle()
--- src/strings_bot.py
+++
@@ -1,8 +0,0 @@
-def help_f():
-    help_txt = """
-    Comandos:
-    /help - Return commands.
-    /start - Start and presentation.
-    /coin <symbol> - Returns the value of the entered currency.
-        """
-    return help_txt
