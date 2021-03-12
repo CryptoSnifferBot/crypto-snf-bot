208210049f5c66aec73f008f991cb8e6f5264353
allanjuliato <allan.juliato@gmail.com>
2021-03-12 16:24:21 -0300

    Tratamento de erro na funçao coin_value

--- src/func_bot.py
+++ src/func_bot.py
@@ -5,17 +5,13 @@ class Func_bot():
         pass
     def coin_value(self, symbol):
         """Call the send_message function to send the coin value and return coin value"""
-        # try:
-        #     id = [c for c in list_coins() if c['symbol'] == symbol.lower()][0]['id']
-        #     result = config.cg.get_price(ids=id, vs_currencies='usd')
-        #     send_message(f'O valor do {id.title()} está ${result[id]["usd"]:,.4f}')
-        # except:
-        #     result = '{}'
-        # return result
-
-        id = [c for c in self.list_coins() if c['symbol'] == symbol.lower()][0]['id']
-        result = config.cg.get_price(ids=id, vs_currencies='usd')
-        self.send_message(f'O valor do {id.title()} está ${result[id]["usd"]:,.4f}')
+        try:
+            id = [c for c in self.list_coins() if c['symbol'] == symbol.lower()][0]['id']
+            result = config.cg.get_price(ids=id, vs_currencies='usd')
+            self.send_message(f'O valor do {id.title()} está ${result[id]["usd"]:,.4f}')
+        except:
+            result = '{}'
+        return result
         
 
     def list_coins(self):
